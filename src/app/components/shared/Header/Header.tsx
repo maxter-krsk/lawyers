5587410d384172210d88e30e3f69338036fceb7f
exrsxe <nigazzz2000@gmail.com>
2026-01-19 18:00:35 +0700

    feat: Готовый адаптированный хедер | глобальный стиль для ссылок при ховере

--- src/app/components/shared/Header/Header.tsx
+++ src/app/components/shared/Header/Header.tsx
@@ -1,9 +1,82 @@
-export default function Header() {
+import Image from "next/image";
+import Link from "next/link";
+import Burger from "./Burger";
+
+const nav: [string, string][] = [
+  ["О компании", "/#about"],
+  ["Команда", "/#team"],
+  ["Преимущества", "/#features"],
+  ["Практики", "/#practices"],
+];
+
+type HeaderProps = {
+  variant: "top" | "side";
+};
+
+export default function Header({ variant }: HeaderProps) {
+  if (variant === "top") {
+    return (
+      <header className="bg-white">
+        <div className="relative container flex min-h-16 items-center justify-end py-14">
+          {/* Центр */}
+          <Link href="/" aria-label="На главную" className="absolute left-1/2 -translate-x-1/2">
+            <Image
+              src="/icons/logos/logo-short.svg"
+              alt="Логотип"
+              width={68}
+              height={46}
+              className="h-auto w-62"
+              priority
+            />
+          </Link>
+
+          <div className="w-42">
+            <Burger className="w-full" />
+          </div>
+        </div>
+      </header>
+    );
+  }
+
   return (
-    <header>
-      <div className="container">
-        <h1 className="text-center text-xl text-black uppercase">Header</h1>
-        <div></div>
+    <header className="top-0 right-0 h-screen bg-white">
+      <div className="container flex h-full flex-col items-center py-20 md:items-start">
+        <nav className="hidden md:block">
+          <ul className="text-18 flex flex-col space-y-10 uppercase">
+            {nav.map(([label, href], index) => (
+              <li key={href} className={index === 1 ? "mb-40" : undefined}>
+                <Link className="hover-link" href={href}>
+                  {label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+
+        <div className="w-full md:hidden">
+          <Burger className="w-full" />
+        </div>
+
+        {/* LOGO */}
+        <div className="my-auto">
+          <Link href="/" aria-label="На главную" className="block">
+            <Image
+              className="hidden h-auto w-[19.188rem] md:block"
+              src="/icons/logos/logo-full.svg"
+              alt="Логотип"
+              width={307}
+              height={177}
+              priority
+            />
+            <Image
+              className="block h-auto w-[68px] md:hidden"
+              src="/icons/logos/logo-short.svg"
+              alt="Логотип"
+              width={68}
+              height={46}
+            />
+          </Link>
+        </div>
       </div>
     </header>
   );
--- src/lib/ui/sheet.tsx
+++ src/lib/ui/sheet.tsx
@@ -1,31 +1,25 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import * as SheetPrimitive from "@radix-ui/react-dialog"
-import { XIcon } from "lucide-react"
+import * as React from "react";
+import * as SheetPrimitive from "@radix-ui/react-dialog";
+import { XIcon } from "lucide-react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 function Sheet({ ...props }: React.ComponentProps<typeof SheetPrimitive.Root>) {
-  return <SheetPrimitive.Root data-slot="sheet" {...props} />
+  return <SheetPrimitive.Root data-slot="sheet" {...props} />;
 }
 
-function SheetTrigger({
-  ...props
-}: React.ComponentProps<typeof SheetPrimitive.Trigger>) {
-  return <SheetPrimitive.Trigger data-slot="sheet-trigger" {...props} />
+function SheetTrigger({ ...props }: React.ComponentProps<typeof SheetPrimitive.Trigger>) {
+  return <SheetPrimitive.Trigger data-slot="sheet-trigger" {...props} />;
 }
 
-function SheetClose({
-  ...props
-}: React.ComponentProps<typeof SheetPrimitive.Close>) {
-  return <SheetPrimitive.Close data-slot="sheet-close" {...props} />
+function SheetClose({ ...props }: React.ComponentProps<typeof SheetPrimitive.Close>) {
+  return <SheetPrimitive.Close data-slot="sheet-close" {...props} />;
 }
 
-function SheetPortal({
-  ...props
-}: React.ComponentProps<typeof SheetPrimitive.Portal>) {
-  return <SheetPrimitive.Portal data-slot="sheet-portal" {...props} />
+function SheetPortal({ ...props }: React.ComponentProps<typeof SheetPrimitive.Portal>) {
+  return <SheetPrimitive.Portal data-slot="sheet-portal" {...props} />;
 }
 
 function SheetOverlay({
@@ -41,7 +35,7 @@ function SheetOverlay({
       )}
       {...props}
     />
-  )
+  );
 }
 
 function SheetContent({
@@ -50,7 +44,7 @@ function SheetContent({
   side = "right",
   ...props
 }: React.ComponentProps<typeof SheetPrimitive.Content> & {
-  side?: "top" | "right" | "bottom" | "left"
+  side?: "top" | "right" | "bottom" | "left";
 }) {
   return (
     <SheetPortal>
@@ -62,7 +56,7 @@ function SheetContent({
           side === "right" &&
             "data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right inset-y-0 right-0 h-full w-3/4 border-l sm:max-w-sm",
           side === "left" &&
-            "data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left inset-y-0 left-0 h-full w-3/4 border-r sm:max-w-sm",
+            "bg-background data-[state=open]:animate-in data-[state=closed]:animate-out fixed relative z-50 flex flex-col gap-4 shadow-lg transition ease-in-out data-[state=closed]:duration-300 data-[state=open]:duration-500",
           side === "top" &&
             "data-[state=closed]:slide-out-to-top data-[state=open]:slide-in-from-top inset-x-0 top-0 h-auto border-b",
           side === "bottom" &&
@@ -72,13 +66,30 @@ function SheetContent({
         {...props}
       >
         {children}
-        <SheetPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-secondary absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none">
-          <XIcon className="size-4" />
-          <span className="sr-only">Close</span>
+        <SheetPrimitive.Close
+          aria-label="Закрыть меню"
+          className="absolute top-20 right-20 inline-flex h-16 w-16 items-center justify-center"
+        >
+          <svg
+            width="15"
+            height="15"
+            viewBox="0 0 15 15"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M13.9792 14.6843L0.315597 1.02048C0.00437028 0.709251 -0.0939499 0.291383 0.0987143 0.098716C0.291378 -0.0939515 0.709239 0.00437036 1.02047 0.315602L14.6841 13.9795C14.9953 14.2907 15.0936 14.7086 14.901 14.9012C14.7083 15.0939 14.2904 14.9956 13.9792 14.6843Z"
+              fill="white"
+            />
+            <path
+              d="M14.6844 1.02055L1.02079 14.6844C0.709565 14.9956 0.291704 15.094 0.0990393 14.9013C-0.093625 14.7086 0.00469502 14.2907 0.315922 13.9795L13.9795 0.315666C14.2908 0.0044342 14.7086 -0.0938876 14.9013 0.0987801C15.0939 0.291448 14.9956 0.709316 14.6844 1.02055Z"
+              fill="white"
+            />
+          </svg>
         </SheetPrimitive.Close>
       </SheetPrimitive.Content>
     </SheetPortal>
-  )
+  );
 }
 
 function SheetHeader({ className, ...props }: React.ComponentProps<"div">) {
@@ -88,7 +99,7 @@ function SheetHeader({ className, ...props }: React.ComponentProps<"div">) {
       className={cn("flex flex-col gap-1.5 p-4", className)}
       {...props}
     />
-  )
+  );
 }
 
 function SheetFooter({ className, ...props }: React.ComponentProps<"div">) {
@@ -98,20 +109,17 @@ function SheetFooter({ className, ...props }: React.ComponentProps<"div">) {
       className={cn("mt-auto flex flex-col gap-2 p-4", className)}
       {...props}
     />
-  )
+  );
 }
 
-function SheetTitle({
-  className,
-  ...props
-}: React.ComponentProps<typeof SheetPrimitive.Title>) {
+function SheetTitle({ className, ...props }: React.ComponentProps<typeof SheetPrimitive.Title>) {
   return (
     <SheetPrimitive.Title
       data-slot="sheet-title"
       className={cn("text-foreground font-semibold", className)}
       {...props}
     />
-  )
+  );
 }
 
 function SheetDescription({
@@ -124,7 +132,7 @@ function SheetDescription({
       className={cn("text-muted-foreground text-sm", className)}
       {...props}
     />
-  )
+  );
 }
 
 export {
@@ -136,4 +144,4 @@ export {
   SheetFooter,
   SheetTitle,
   SheetDescription,
-}
+};
