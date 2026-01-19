c9a3b47427f9467d183dd74c524af1e3da11b76a
exrsxe <nigazzz2000@gmail.com>
2026-01-20 01:00:51 +0700

    feat: Блок 'О нас' | Анимация аккордеона | Начало адаптации

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Hero } from "./components/sections/Hero/Hero";
 import { About } from "./components/sections/About/About";
+import { Practices } from "./components/sections/Practices/Practices";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <About />
+      <Practices />
     </>
   );
 }
