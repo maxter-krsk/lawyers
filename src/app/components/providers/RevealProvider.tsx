09d0510d5ca5586e86c7f75aa24ebcb20d7dd3a6
exrsxe <nigazzz2000@gmail.com>
2026-02-05 10:28:02 +0700

    fix: Reveal срабатывает каждый раз при заходе на страницу | Карта прогружается каждый раз

--- src/app/components/providers/RevealProvider.tsx
+++ src/app/components/providers/RevealProvider.tsx
@@ -1,12 +1,16 @@
 "use client";
 
 import { useEffect } from "react";
+import { usePathname } from "next/navigation";
 import { initReveal } from "../../../lib/utils/reveal";
 
 export function RevealProvider() {
+  const pathname = usePathname();
+
   useEffect(() => {
-    initReveal();
-  }, []);
+    const cleanup = initReveal();
+    return cleanup;
+  }, [pathname]);
 
   return null;
 }
